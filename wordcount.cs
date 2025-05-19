e987fe83a26ad298a9e691ae303eaecd984b2b49
ayusha5600 <aayusha.kattel@sa.stud.vu.lt>
2025-05-19 23:59:18 +0300

    Update wordcount.cs

--- wordcount.cs
+++ wordcount.cs
@@ -1,1 +1,15 @@
+namespace Shared
+{
+    public class WordCount
+    {
+        public string FileName { get; set; }
+        public string Word { get; set; }
+        public int Count { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FileName}:{Word}:{Count}";
+        }
+    }
+}
 
